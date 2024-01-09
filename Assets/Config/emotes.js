0abc75fcffc4e187120692eaa3a8db388f88f568
ironweb10 <97882381+ironweb10@users.noreply.github.com>
2024-01-09 11:10:22 +0100

    Add files via upload

---
+++ Assets/Config/emotes.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MESSAGE: {
+    y: "<a:Verified:1105805443688841226>", // yes 
+    x: "<a:error:1105805398960771104>", // no
+    i: "<a:error:1105805398960771104>", // info
+    l: "<a:loading:1164163559723384852>" // loading
+  }
+}
