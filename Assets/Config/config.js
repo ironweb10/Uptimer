ff14448bded1dc0996494ac9736c1a7e844016c5
ironweb10 <97882381+ironweb10@users.noreply.github.com>
2024-01-09 11:10:00 +0100

    Create config.js

---
+++ Assets/Config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  TOKEN: process.env.TOKEN || "",
+  MONGO_DB: process.env.MONGO_DB || "",
+  CLIENT_ID: process.env.CLIENT_ID || "",
+  OWNERS: ["846370965994864651"],
+  SUPPORT_SERVER: "https://discord.gg/5phHVahD3A",
+  CHANNELS: {
+    COMMANDS_LOGS: "",
+    ERROR_COMMAND_LOGS: ""
+  }
+}
